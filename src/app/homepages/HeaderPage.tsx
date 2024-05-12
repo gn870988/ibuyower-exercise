9270a295e962f49681c9668965536406e8d12aae
Vincent.yu <gn870988@gmail.com>
2024-05-12 13:55:47 +0800

    [feat] add header page

---
+++ src/app/homepages/HeaderPage.tsx
@@ -0,0 +1,8 @@
+export default function HeaderPage() {
+  return (
+    <div>
+      <p className="text-center text-2xl font-bold">Best Seller Gaming PC</p>
+      <p className="text-center text-lg font-bold">Prebuilt & Custom</p>
+    </div>
+  );
+}
