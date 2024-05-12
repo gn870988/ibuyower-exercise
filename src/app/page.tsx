5f5e49bd076f1a18fd5ebe7995eb35d4a020c8fe
Vincent.yu <gn870988@gmail.com>
2024-05-12 14:12:18 +0800

    [feat] add auction cards and mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import React from "react";
 import HeaderPage from "./homepages/HeaderPage";
+import mockdata from "@/mockdata/data.json";
+import { Auction } from "@/types";
+import AuctionContainer from "./auctions/AuctionContainer";
+import { register } from "swiper/element/bundle";
+register();
+
+type State = {
+  auctions: Auction[];
+};
+
+const data: State = {
+  auctions: mockdata,
+};
 
 export default function Home() {
   return (
     <>
       <HeaderPage />
+      <AuctionContainer auctions={data.auctions} />
     </>
   );
 }
